de36d138c57f0081d075b4ed17230ab0e20ec711
LillyJadeKatrin <lilly.kitty.1988@gmail.com>
2023-04-15 11:27:39 -0400

    Added PingRichPresence to AchievementManager
    PingRichPresence makes a "ping" API request to the RetroAchievements website with the provided RichPresence string parameter. While there has been talk about tying ping in with session, in its current state the primary purpose of ping is to send the player's Rich Presence to the website.

--- Source/Core/Core/AchievementManager.h
+++ Source/Core/Core/AchievementManager.h
@@ -19,6 +19,8 @@
 #include "Common/WorkQueueThread.h"
 
 using AchievementId = u32;
+constexpr int RP_SIZE = 65536;
+using RichPresence = std::array<char, RP_SIZE>;
 
 class AchievementManager
 {
@@ -64,6 +66,8 @@ private:
 
   ResponseType AwardAchievement(AchievementId achievement_id);
   ResponseType SubmitLeaderboard(AchievementId leaderboard_id, int value);
+  ResponseType PingRichPresence(RichPresence rich_presence);
+
   template <typename RcRequest, typename RcResponse>
   ResponseType Request(RcRequest rc_request, RcResponse* rc_response,
                        const std::function<int(rc_api_request_t*, const RcRequest*)>& init_request,
