dolphin-emu/dolphin

Added RAIntegration to AchievementManager
Added the necessary wiring for RAIntegration to operate to AchievementManager, wrapped with the ENABLE_RAINTEGRATION flag.

Source/Core/DolphinQt/MainWindow.cpp
@@ -226,6 +226,7 @@ MainWindow::MainWindow(std::unique_ptr<BootParameters> boot_parameters,
 #ifdef USE_RETRO_ACHIEVEMENTS
   // This has to be done before CreateComponents() so it's initialized.
   AchievementManager::GetInstance()->Init();
+  MainWindowChanged((HANDLE)winId());
 #endif  // USE_RETRO_ACHIEVEMENTS
 
   CreateComponents();
@@ -533,6 +534,7 @@ void MainWindow::ConnectMenuBar()
   connect(m_menu_bar, &MenuBar::ShowFIFOPlayer, this, &MainWindow::ShowFIFOPlayer);
   connect(m_menu_bar, &MenuBar::ShowSkylanderPortal, this, &MainWindow::ShowSkylanderPortal);
   connect(m_menu_bar, &MenuBar::ConnectWiiRemote, this, &MainWindow::OnConnectWiiRemote);
+  connect(m_menu_bar, &MenuBar::ActivateRAMenuItem, this, &MainWindow::ActivateRAMenuItem);
 
   // Movie
   connect(m_menu_bar, &MenuBar::PlayRecording, this, &MainWindow::OnPlayRecording);
@@ -1138,6 +1140,8 @@ void MainWindow::ShowRenderWidget()
     m_stack->repaint();
 
     Host::GetInstance()->SetRenderFocus(isActiveWindow());
+
+    MainWindowChanged((HANDLE)winId());
   }
   else
   {
@@ -1146,6 +1150,8 @@ void MainWindow::ShowRenderWidget()
 
     m_render_widget->showNormal();
     m_render_widget->restoreGeometry(m_render_widget_geometry);
+
+    MainWindowChanged((HANDLE)m_render_widget->winId());
   }
 }
 
@@ -1189,6 +1195,8 @@ void MainWindow::HideRenderWidget(bool reinit, bool is_exit)
     g_controller_interface.ChangeWindow(GetWindowSystemInfo(windowHandle()).render_window,
                                         is_exit ? ControllerInterface::WindowChangeReason::Exit :
                                                   ControllerInterface::WindowChangeReason::Other);
+
+    MainWindowChanged((HANDLE)winId());
   }
 }
 
@@ -1419,6 +1427,15 @@ void MainWindow::PerformOnlineUpdate(const std::string& region)
 void MainWindow::BootWiiSystemMenu()
 {
   StartGame(std::make_unique<BootParameters>(BootParameters::NANDTitle{Titles::SYSTEM_MENU}));
+}
+
+void MainWindow::MainWindowChanged(void* new_handle)
+{
+#ifdef USE_RETRO_ACHIEVEMENTS
+#ifdef ENABLE_RAINTEGRATION
+  AchievementManager::GetInstance()->MainWindowChanged(new_handle);
+#endif  // ENABLE_RAINTEGRATION
+#endif  // USE_RETRO_ACHIEVEMENTS
 }
 
 void MainWindow::NetPlayInit()
@@ -1863,6 +1880,15 @@ void MainWindow::OnConnectWiiRemote(int id)
       wm->Activate(!wm->IsConnected());
     }
   });
+}
+
+void MainWindow::ActivateRAMenuItem(int id)
+{
+#ifdef USE_RETRO_ACHIEVEMENTS
+#ifdef ENABLE_RAINTEGRATION
+  AchievementManager::GetInstance()->ActivateMenuItem(id);
+#endif  // ENABLE_RAINTEGRATION
+#endif  // USE_RETRO_ACHIEVEMENTS
 }
 
 void MainWindow::ShowMemcardManager()

Source/Core/DolphinQt/MenuBar.cpp
@@ -19,6 +19,7 @@
 #include "Common/FileUtil.h"
 #include "Common/StringUtil.h"
 
+#include "Core/AchievementManager.h"
 #include "Core/Boot/Boot.h"
 #include "Core/CommonTitles.h"
 #include "Core/Config/MainSettings.h"
@@ -226,6 +227,37 @@ void MenuBar::AddToolsMenu()
 
   tools_menu->addAction(tr("Start &NetPlay..."), this, &MenuBar::StartNetPlay);
   tools_menu->addAction(tr("Browse &NetPlay Sessions...."), this, &MenuBar::BrowseNetPlay);
+
+#ifdef USE_RETRO_ACHIEVEMENTS
+#ifdef ENABLE_RAINTEGRATION
+  tools_menu->addSeparator();
+
+  if (AchievementManager::GetInstance()->IsDLLEnabled())
+  {
+    QMenu* ra_dev_ipl = tools_menu->addMenu(tr("Achievement Development"));
+
+    const auto items = AchievementManager::GetInstance()->GetMenuItems();
+    for (const auto& [id, title, checked] : items)
+    {
+      if (id == 0)
+      {
+        ra_dev_ipl->addSeparator();
+        continue;
+      }
+
+      QAction* raAction = ra_dev_ipl->addAction(QString::fromUtf8(title));
+      if (checked)
+      {
+        raAction->setCheckable(true);
+        raAction->setChecked(checked);
+      }
+
+      connect(raAction, &QAction::triggered, this,
+              [this, id = id]() { emit ActivateRAMenuItem(id); });
+    }
+  }
+#endif  // ENABLE_RAINTEGRATION
+#endif  // USE_RETRO_ACHIEVEMENTS
 
   tools_menu->addSeparator();
 

LillyJadeKatrin/dolphin-retroachievements

Source/Core/DolphinQt/MainWindow.cpp
@@ -226,6 +226,7 @@ MainWindow::MainWindow(std::unique_ptr<BootParameters> boot_parameters,
 #ifdef USE_RETRO_ACHIEVEMENTS
   // This has to be done before CreateComponents() so it's initialized.
   AchievementManager::GetInstance()->Init();
+  MainWindowChanged((HANDLE)winId());
 #endif  // USE_RETRO_ACHIEVEMENTS
 
   CreateComponents();
@@ -533,6 +534,7 @@ void MainWindow::ConnectMenuBar()
   connect(m_menu_bar, &MenuBar::ShowFIFOPlayer, this, &MainWindow::ShowFIFOPlayer);
   connect(m_menu_bar, &MenuBar::ShowSkylanderPortal, this, &MainWindow::ShowSkylanderPortal);
   connect(m_menu_bar, &MenuBar::ConnectWiiRemote, this, &MainWindow::OnConnectWiiRemote);
+  connect(m_menu_bar, &MenuBar::ActivateRAMenuItem, this, &MainWindow::ActivateRAMenuItem);
 
   // Movie
   connect(m_menu_bar, &MenuBar::PlayRecording, this, &MainWindow::OnPlayRecording);
@@ -1138,6 +1140,8 @@ void MainWindow::ShowRenderWidget()
     m_stack->repaint();
 
     Host::GetInstance()->SetRenderFocus(isActiveWindow());
+
+    MainWindowChanged((HANDLE)winId());
   }
   else
   {
@@ -1146,6 +1150,8 @@ void MainWindow::ShowRenderWidget()
 
     m_render_widget->showNormal();
     m_render_widget->restoreGeometry(m_render_widget_geometry);
+
+    MainWindowChanged((HANDLE)m_render_widget->winId());
   }
 }
 
@@ -1189,6 +1195,8 @@ void MainWindow::HideRenderWidget(bool reinit, bool is_exit)
     g_controller_interface.ChangeWindow(GetWindowSystemInfo(windowHandle()).render_window,
                                         is_exit ? ControllerInterface::WindowChangeReason::Exit :
                                                   ControllerInterface::WindowChangeReason::Other);
+
+    MainWindowChanged((HANDLE)winId());
   }
 }
 
@@ -1419,6 +1427,15 @@ void MainWindow::PerformOnlineUpdate(const std::string& region)
 void MainWindow::BootWiiSystemMenu()
 {
   StartGame(std::make_unique<BootParameters>(BootParameters::NANDTitle{Titles::SYSTEM_MENU}));
+}
+
+void MainWindow::MainWindowChanged(void* new_handle)
+{
+#ifdef USE_RETRO_ACHIEVEMENTS
+#ifdef _WIN32
+  AchievementManager::GetInstance()->MainWindowChanged(new_handle);
+#endif  // _WIN32
+#endif  // USE_RETRO_ACHIEVEMENTS
 }
 
 void MainWindow::NetPlayInit()
@@ -1863,6 +1880,15 @@ void MainWindow::OnConnectWiiRemote(int id)
       wm->Activate(!wm->IsConnected());
     }
   });
+}
+
+void MainWindow::ActivateRAMenuItem(int id)
+{
+#ifdef USE_RETRO_ACHIEVEMENTS
+#ifdef _WIN32
+  AchievementManager::GetInstance()->ActivateMenuItem(id);
+#endif  // _WIN32
+#endif  // USE_RETRO_ACHIEVEMENTS
 }
 
 void MainWindow::ShowMemcardManager()

Source/Core/DolphinQt/MenuBar.cpp
@@ -19,6 +19,7 @@
 #include "Common/FileUtil.h"
 #include "Common/StringUtil.h"
 
+#include "Core/AchievementManager.h"
 #include "Core/Boot/Boot.h"
 #include "Core/CommonTitles.h"
 #include "Core/Config/MainSettings.h"
@@ -226,6 +227,37 @@ void MenuBar::AddToolsMenu()
 
   tools_menu->addAction(tr("Start &NetPlay..."), this, &MenuBar::StartNetPlay);
   tools_menu->addAction(tr("Browse &NetPlay Sessions...."), this, &MenuBar::BrowseNetPlay);
+
+#ifdef USE_RETRO_ACHIEVEMENTS
+#ifdef _WIN32
+  tools_menu->addSeparator();
+
+  if (AchievementManager::GetInstance()->IsDLLEnabled())
+  {
+    QMenu* ra_dev_ipl = tools_menu->addMenu(tr("Achievement Development"));
+
+    const auto items = AchievementManager::GetInstance()->GetMenuItems();
+    for (const auto& [id, title, checked] : items)
+    {
+      if (id == 0)
+      {
+        ra_dev_ipl->addSeparator();
+        continue;
+      }
+
+      QAction* raAction = ra_dev_ipl->addAction(QString::fromUtf8(title));
+      if (checked)
+      {
+        raAction->setCheckable(true);
+        raAction->setChecked(checked);
+      }
+
+      connect(raAction, &QAction::triggered, this,
+              [this, id = id]() { emit ActivateRAMenuItem(id); });
+    }
+  }
+#endif  // _WIN32
+#endif  // USE_RETRO_ACHIEVEMENTS
 
   tools_menu->addSeparator();
 