ce1aa425732037950e07c9afa745c01e22f6f10a
LillyJadeKatrin <lilly.kitty.1988@gmail.com>
2023-06-15 03:57:58 -0400

    Added player and game badges to Achievements dialog header
    Provided badges are turned on, if there's a player logged in their RetroAchievements icon will appear next to their player info in the header of the Achievements dialog. If they're playing a game, so will the icon for the game.

--- Source/Core/DolphinQt/Achievements/AchievementHeaderWidget.cpp
+++ Source/Core/DolphinQt/Achievements/AchievementHeaderWidget.cpp
@@ -20,6 +20,7 @@
 #include <rcheevos/include/rc_runtime.h>
 
 #include "Core/AchievementManager.h"
+#include "Core/Config/AchievementSettings.h"
 #include "Core/Core.h"
 
 #include "DolphinQt/QtUtils/ModalMessageBox.h"
@@ -29,8 +30,10 @@
 
 AchievementHeaderWidget::AchievementHeaderWidget(QWidget* parent) : QWidget(parent)
 {
+  m_user_icon = new QLabel();
   m_user_name = new QLabel();
   m_user_points = new QLabel();
+  m_game_icon = new QLabel();
   m_game_name = new QLabel();
   m_game_points = new QLabel();
   m_game_progress_hard = new QProgressBar();
@@ -80,6 +83,25 @@ void AchievementHeaderWidget::UpdateData()
     return;
   }
 
+  AchievementManager::BadgeStatus player_badge =
+      AchievementManager::GetInstance()->GetPlayerBadge();
+  if (Config::Get(Config::RA_BADGES_ENABLED) && player_badge.loaded)
+  {
+    QImage i_user_icon{};
+    i_user_icon.loadFromData(player_badge.badge.begin()._Ptr, (int)player_badge.badge.size());
+    m_user_icon->setPixmap(QPixmap::fromImage(i_user_icon)
+                               .scaled(64, 64, Qt::KeepAspectRatio, Qt::SmoothTransformation));
+    m_user_icon->adjustSize();
+    m_user_icon->setStyleSheet(QString::fromStdString("border: 4px solid transparent"));
+    m_user_icon->setVisible(true);
+  }
+  else
+  {
+    m_user_icon->setVisible(false);
+    m_user_icon->clear();
+    m_user_icon->setText({});
+  }
+
   QString user_name =
       QString::fromStdString(AchievementManager::GetInstance()->GetPlayerDisplayName());
   m_user_name->setText(user_name);
@@ -92,6 +114,24 @@ void AchievementHeaderWidget::UpdateData()
     return;
   }
 
+  AchievementManager::BadgeStatus game_badge =
+      AchievementManager::GetInstance()->GetGameBadge();
+  if (Config::Get(Config::RA_BADGES_ENABLED) && game_badge.loaded)
+  {
+    QImage i_game_icon{};
+    i_game_icon.loadFromData(game_badge.badge.begin()._Ptr, (int)game_badge.badge.size());
+    m_game_icon->setPixmap(QPixmap::fromImage(i_game_icon)
+                               .scaled(64, 64, Qt::KeepAspectRatio, Qt::SmoothTransformation));
+    m_game_icon->adjustSize();
+    m_game_icon->setStyleSheet(QString::fromStdString("border: 4px solid transparent"));
+    m_game_icon->setVisible(true);
+  }
+  else
+  {
+    m_game_icon->setVisible(false);
+    m_game_icon->clear();
+    m_game_icon->setText({});
+  }
   AchievementManager::PointSpread point_spread = AchievementManager::GetInstance()->TallyScore();
   m_game_name->setText(
       QString::fromStdString(AchievementManager::GetInstance()->GetGameDisplayName()));
