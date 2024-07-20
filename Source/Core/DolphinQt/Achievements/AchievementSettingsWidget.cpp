97048f37cd279e3a1181bddedb9b03fc6570fb5f
LillyJadeKatrin <lilly.kitty.1988@gmail.com>
2024-07-19 22:18:13 -0400

    Load RA_Integration.DLL At Init If Present

--- Source/Core/DolphinQt/Achievements/AchievementSettingsWidget.cpp
+++ Source/Core/DolphinQt/Achievements/AchievementSettingsWidget.cpp
@@ -255,7 +255,7 @@ void AchievementSettingsWidget::ToggleRAIntegration()
 
   auto& instance = AchievementManager::GetInstance();
   if (Config::Get(Config::RA_ENABLED))
-    instance.Init();
+    instance.Init(reinterpret_cast<void*>(winId()));
   else
     instance.Shutdown();
   if (Config::Get(Config::RA_HARDCORE_ENABLED))
